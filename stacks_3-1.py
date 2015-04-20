5ecd4ccc734ca311c3a5f51413d811d5b993bad9
wiedld <dlw405@gmail.com>
2015-04-19 20:19:26 -0700

    made own linked list class, with array which represents slots in memory

---
+++ stacks_3-1.py
@@ -0,0 +1,62 @@
+# PROBLEM: Describe how you could use a single array to implement three stacks
+
+# PROPOSED SOLUTION:
+# - treat a single array like memory slots.
+# - Then each stack is similiar to a linked list.
+#     - one mem slot (array position) is the data.
+#     - adjacent mem slot (array position) is the pointer to the previous item
+#     - to create the Filo aspect of a stack:
+#         - the pointer goes from the next added value, to the previous value using a singly linked .lower (e.g. .next).
+#         new_value.lower = previous_value
+
+###############################################################
+
+# As a learning experience, will try to make one.
+# as I believe this is how linked lists work with memory slots.
+
+
+# mem_array = []
+
+class stack(object):
+    # global mem_array
+    mem_array = []
+
+    def __init__(self):
+        pointer = ""
+        # stack ends with a pointer to none at idx = last_added.
+        self.mem_array.extend(["None", pointer])
+        # update last added for new value
+        self.last_added_idx = len(self.mem_array)-2
+
+    def add(self, data):
+        # points to previously added
+        pointer = self.last_added_idx
+        self.mem_array.extend([data, pointer])
+        # update last added for new value
+        self.last_added_idx = len(self.mem_array)-2
+
+
+
+# create a stack.
+test_stack = stack()
+test_stack.add("first_1")
+test_stack.add("second_1")
+print test_stack.mem_array
+
+# create another stack, and should build into single array
+test_stack_2 = stack()
+test_stack_2.add("first_2")
+test_stack_2.add("second_2")
+print test_stack_2.mem_array
+
+# what if I add to the original stack.
+# Does it correctly update the pointer in the array?
+test_stack.add("third_1")
+test_stack_2.add("third_2")
+print test_stack.mem_array
+
+
+
+
+
+
